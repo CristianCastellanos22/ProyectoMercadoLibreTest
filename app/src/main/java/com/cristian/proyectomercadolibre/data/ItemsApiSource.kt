b25cb51f1b3b63a79a570cafb24a431e4109ca7a
CristianCastellanos <cristiancastellanos7@gmail.com>
2021-10-21 20:10:26 -0500

    create the package, class and interface to manage the data section

--- app/src/main/java/com/cristian/proyectomercadolibre/data/ItemsApiSource.kt
+++ app/src/main/java/com/cristian/proyectomercadolibre/data/ItemsApiSource.kt
@@ -1,4 +1,7 @@
 package com.cristian.proyectomercadolibre.data
 
+import com.cristian.proyectomercadolibre.models.Item
+
 interface ItemsApiSource {
+    suspend fun getItems(): List<Item>
 }
--- app/src/main/java/com/cristian/proyectomercadolibre/data/ItemsApiSourceAdapter.kt
+++ app/src/main/java/com/cristian/proyectomercadolibre/data/ItemsApiSourceAdapter.kt
@@ -1,4 +1,10 @@
 package com.cristian.proyectomercadolibre.data
 
-class ItemsApiSourceAdapter {
+import com.cristian.proyectomercadolibre.domain.ItemsRepository
+import com.cristian.proyectomercadolibre.models.Item
+
+class ItemsApiSourceAdapter(private val itemsApiSource: ItemsApiSource): ItemsRepository {
+    override suspend fun getItems(): List<Item> {
+        return itemsApiSource.getItems()
+    }
 }
