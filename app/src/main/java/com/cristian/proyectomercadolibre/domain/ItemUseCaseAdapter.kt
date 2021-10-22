edbd1502b9b965b29549678f40ebae787c5280fa
CristianCastellanos <cristiancastellanos7@gmail.com>
2021-10-21 22:08:31 -0500

    add parameter item: String

--- app/src/main/java/com/cristian/proyectomercadolibre/domain/ItemUseCaseAdapter.kt
+++ app/src/main/java/com/cristian/proyectomercadolibre/domain/ItemUseCaseAdapter.kt
@@ -4,5 +4,5 @@ import com.cristian.proyectomercadolibre.framework.ui.ItemsUseCase
 import com.cristian.proyectomercadolibre.models.Item
 
 class ItemUseCaseAdapter(private val itemsRepository: ItemsRepository): ItemsUseCase {
-    override suspend fun getItem(): List<Item> = itemsRepository.getItems()
+    override suspend fun getItem(item: String): List<Item> = itemsRepository.getItems(item)
 }
--- app/src/main/java/com/cristian/proyectomercadolibre/domain/ItemsRepository.kt
+++ app/src/main/java/com/cristian/proyectomercadolibre/domain/ItemsRepository.kt
@@ -3,5 +3,5 @@ package com.cristian.proyectomercadolibre.domain
 import com.cristian.proyectomercadolibre.models.Item
 
 interface ItemsRepository {
-    suspend fun getItems(): List<Item>
+    suspend fun getItems(item: String): List<Item>
 }
