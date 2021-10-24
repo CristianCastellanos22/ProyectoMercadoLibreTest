1b4941605d06577c405824bc4c6f55a0df985daf
CristianCastellanos <cristiancastellanos7@gmail.com>
2021-10-24 01:46:47 -0500

    Create class to manage inject dependecy = ItemModule.kt and ItemComponents.kt, the Servicesmodule is the instances of retrofit, add header and init de instance

--- app/src/main/java/com/cristian/proyectomercadolibre/framework/di/ItemsModule.kt
+++ app/src/main/java/com/cristian/proyectomercadolibre/framework/di/ItemsModule.kt
@@ -1,4 +1,58 @@
 package com.cristian.proyectomercadolibre.framework.di
 
-class ItemsModule {
+import android.content.Context
+import com.cristian.proyectomercadolibre.data.categories.CategoriesRepositoryAdapter
+import com.cristian.proyectomercadolibre.data.categoriesDetails.CategoriesDetailsRepositoryAdapter
+import com.cristian.proyectomercadolibre.data.items.ItemsRepositoryAdapter
+import com.cristian.proyectomercadolibre.domain.categories.CategoriesUseCaseAdapter
+import com.cristian.proyectomercadolibre.domain.categoriesDetails.CategoriesDetailsUseCaseAdapter
+import com.cristian.proyectomercadolibre.domain.items.ItemUseCaseAdapter
+import com.cristian.proyectomercadolibre.framework.data_source.api.categories.CategoriesApiSourceAdapter
+import com.cristian.proyectomercadolibre.framework.data_source.api.categoriesDetails.CategoriesDetailsApiSourceAdapter
+import com.cristian.proyectomercadolibre.framework.data_source.api.items.ItemsApiSourceAdapter
+import com.cristian.proyectomercadolibre.framework.ui.categories.CategoriesViewModelFactory
+import com.cristian.proyectomercadolibre.framework.ui.categoriesDetails.CategoriesDetailsViewModelFactory
+import com.cristian.proyectomercadolibre.framework.ui.items.ItemsViewModelFactory
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class ItemsModule(private val context: Context) {
+    @Provides
+    @Singleton
+    fun provideItems() = ItemsViewModelFactory(provideUseCase())
+
+    @Provides
+    @Singleton
+    fun provideUseCase() = ItemUseCaseAdapter(
+        ItemsRepositoryAdapter(
+            ItemsApiSourceAdapter(ServicesModule.getItemsServices())
+        )
+    )
+
+    @Provides
+    @Singleton
+    fun provideCategories() = CategoriesViewModelFactory(categoriesProvide())
+
+    @Provides
+    @Singleton
+    fun categoriesProvide() = CategoriesUseCaseAdapter(
+        CategoriesRepositoryAdapter(
+            CategoriesApiSourceAdapter(ServicesModule.getCategories())
+        )
+    )
+
+    @Provides
+    @Singleton
+    fun provideCategoriesDetails() = CategoriesDetailsViewModelFactory(categoriesDetailsProvide())
+
+    @Provides
+    @Singleton
+    fun categoriesDetailsProvide() = CategoriesDetailsUseCaseAdapter(
+        CategoriesDetailsRepositoryAdapter(
+            CategoriesDetailsApiSourceAdapter(ServicesModule.getCategoriesDetails())
+        )
+    )
+
 }
--- app/src/main/java/com/cristian/proyectomercadolibre/framework/di/ServicesModule.kt
+++ app/src/main/java/com/cristian/proyectomercadolibre/framework/di/ServicesModule.kt
@@ -1,7 +1,10 @@
 package com.cristian.proyectomercadolibre.framework.di
 
-import com.cristian.proyectomercadolibre.framework.data_source.api.ItemsServices
+import com.cristian.proyectomercadolibre.framework.data_source.api.categories.CategoriesServices
+import com.cristian.proyectomercadolibre.framework.data_source.api.categoriesDetails.CategoriesDetailsServices
+import com.cristian.proyectomercadolibre.framework.data_source.api.items.ItemsServices
 import com.cristian.proyectomercadolibre.utils.ConstantsServices
+import com.google.gson.GsonBuilder
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
@@ -11,17 +14,34 @@ object ServicesModule {
     private val headerInterceptor = Interceptor { chain ->
         var request = chain.request()
 
-        request = request.newBuilder().addHeader("Authorization", "Bearer '$'ACCESS_TOKEN").build()
+        request = request.newBuilder()
+            .addHeader("Authorization", "Bearer \$ACCESS_TOKEN")
+            .build()
+
+        println("Request: ${request.headers()}")
+
         val response = chain.proceed(request)
+        print("url final $response")
         response
     }
 
     private val okHttp = OkHttpClient.Builder().addInterceptor(headerInterceptor)
 
-    private val retrofit: Retrofit = Retrofit.Builder().baseUrl(ConstantsServices.URL_BASE).addConverterFactory(GsonConverterFactory.create())
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(ConstantsServices.URL_BASE)
+        .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+        .client(okHttp.build())
         .build()
 
     fun getItemsServices(): ItemsServices {
         return retrofit.create(ItemsServices::class.java)
+    }
+
+    fun getCategories(): CategoriesServices {
+        return retrofit.create(CategoriesServices::class.java)
+    }
+
+    fun getCategoriesDetails(): CategoriesDetailsServices {
+        return retrofit.create(CategoriesDetailsServices::class.java)
     }
 }
